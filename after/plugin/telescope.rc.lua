733d32227bbaff7b2350e944ab9111e09b24e0b0
Victor Cabello <vmeca87@gmail.com>
2023-02-22 02:36:14 -0600

    fixing telescope

--- after/plugin/telescope.rc.lua
+++ after/plugin/telescope.rc.lua
@@ -16,6 +16,10 @@ telescope.setup {
         ["q"] = actions.close
       },
     },
+    file_ignore_patterns = {
+      "node_modules\\",
+      ".git\\"
+    }
   },
   extensions = {
     file_browser = {
