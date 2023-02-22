f8ee63c619cc7e32603d7ab4dfc1b7292df3b184
Victor Cabello <vmeca87@gmail.com>
2023-02-22 14:18:07 -0600

    add maps

--- lua/maps.lua
+++ lua/maps.lua
@@ -1,3 +1,22 @@
 local keymap = vim.keymap
 
+-- File management
 keymap.set('n', '<leader>w', ':w<CR>')
+
+-- Switch CWD to the directory of the open buffer
+keymap.set('n', '<leader>cd', ':cd %:p:h<cr>:pwd<cr>')
+
+-- Search
+keymap.set('n', '<space>', '/')
+keymap.set('n', '<leader><cr>', ':noh<cr>')
+
+-- Move on splits
+keymap.set('n', '<C-j>', '<C-W>j')
+keymap.set('n', '<C-k>', '<C-W>k')
+keymap.set('n', '<C-h>', '<C-W>h')
+keymap.set('n', '<C-l>', '<C-W>l')
+
+-- Useful mappings for managing tabs
+keymap.set('n', '<leader>tn', ':tabnew<cr>') 
+keymap.set('n', '<leader>to', ':tabonly<cr>') 
+keymap.set('n', '<leader>tc', ':tabclose<cr>') 
