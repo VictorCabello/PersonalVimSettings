2bf9bdb8b6f3549ad2f31dc5f8a9d7146211f8e8
Victor Cabello <vmeca87@gmail.com>
2023-02-24 00:41:37 -0600

    configuer lsp and indent

--- lua/base.lua
+++ lua/base.lua
@@ -5,7 +5,7 @@ vim.opt.encoding = 'utf-8'
 vim.opt.fileencoding = 'utf-8'
 
 vim.wo.number = true
-
+vim.opt.termguicolors = true
 vim.opt.title = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
@@ -18,8 +18,8 @@ vim.opt.expandtab = true
 vim.opt.scrolloff = 10
 vim.opt.shell = 'powershell'
 vim.opt.shellcmdflag = '-nologo -noprofile -ExecutionPolicy RemoteSigned -command'
-vim.opt.shellquote='"'
-vim.opt.shellxquote=''
+vim.opt.shellquote = '"'
+vim.opt.shellxquote = ''
 vim.opt.backupskip = { '/tmp/*', '/private/tmp/*' }
 vim.opt.inccommand = 'split'
 vim.opt.ignorecase = true -- Case insensitive searching UNLESS /C or capital in search
@@ -31,7 +31,7 @@ vim.opt.wrap = false -- No Wrap lines
 vim.opt.backspace = { 'start', 'eol', 'indent' }
 vim.opt.path:append { '**' } -- Finding files - Search down into subfolders
 vim.opt.wildignore:append { '*/node_modules/*' }
-vim.opt.relativenumber=true
+vim.opt.relativenumber = true
 
 -- Undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
@@ -39,8 +39,8 @@ vim.cmd([[let &t_Ce = "\e[4:0m"]])
 
 -- Turn off paste mode when leaving insert
 vim.api.nvim_create_autocmd("InsertLeave", {
-  pattern = '*',
-  command = "set nopaste"
+	pattern = '*',
+	command = "set nopaste"
 })
 
 -- Add asterisks in block comments
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -49,5 +49,6 @@ packer.startup(function(use)
 
   use 'lewis6991/gitsigns.nvim'
   use 'dinhhuy258/git.nvim' -- For git blame & browse
-  use 'sei40kr/nvimacs'
+  use 'editorconfig/editorconfig-vim'
+  use "lukas-reineke/indent-blankline.nvim"
 end)
