4a66f64ff20afcacb4c11c6ec5afeb8d89f39fec
Victor Cabello <vmeca87@gmail.com>
2024-07-07 18:57:52 -0600

    Add snippets

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -8,7 +8,8 @@ return {
     'hrsh7th/cmp-path',
     'hrsh7th/cmp-cmdline',
     'hrsh7th/cmp-vsnip',
-    'hrsh7th/vim-vsnip'
+    'hrsh7th/vim-vsnip',
+    "rafamadriz/friendly-snippets"
   },
   config = function()
     local cmp = require'cmp'
