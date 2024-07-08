9f5eb9551980a22f70d8c2c04ee3ecd0991de67d
Victor Cabello <vmeca87@gmail.com>
2024-07-07 18:24:51 -0600

    add telescope fzf

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,18 @@
 
 return {
-	'nvim-telescope/telescope.nvim', tag = '0.1.8',
+	'nvim-telescope/telescope.nvim',
+  tag = '0.1.8',
+  dependencies = {
+    { 
+      'nvim-telescope/telescope-fzf-native.nvim', 
+      build = 'make',
+      config = function ()
+        local telescope = require('telescope')
+        telescope.load_extension("fzf")
+      end
+    },
+    "nvim-tree/nvim-web-devicons"
+  },
   config = function()
     local builtin = require 'telescope.builtin'
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
@@ -13,5 +25,6 @@ return {
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+
   end
 }
