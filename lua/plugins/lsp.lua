6433ccceab11e8d83dbaf44c1e31de8f6d560e62
Victor Cabello <vmeca87@gmail.com>
2024-07-14 14:09:40 -0600

    Remove unussed variable

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -86,9 +86,7 @@ return {
     lspconfig.gdscript.setup{
       capabilities = capabilities,
     }
-    
     local lspconfig = require('lspconfig')
-    local configs = require('lspconfig/configs')
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities.textDocument.completion.completionItem.snippetSupport = true
 
