ed6a49e2f7b8d8d81c947cb73de636d9f3489148
Victor Cabello <vmeca87@gmail.com>
2025-04-27 13:12:54 -0600

    keymap <leader>w to save file

--- lua/config/opts.lua
+++ lua/config/opts.lua
@@ -41,6 +41,8 @@ end, { desc = 'Go to next [D]iagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+vim.keymap.set('n', '<leader>w', ':w<cr>', { desc = 'save file'} )
+vim.keymap.set('i', '<leader>w', '<C-o>:w<cr>', { desc = 'save file'} )
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
