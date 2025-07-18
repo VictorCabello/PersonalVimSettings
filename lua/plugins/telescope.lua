8912d33950fad5c619210b88c175169454fb564a
Victor Cabello <vmeca87@gmail.com>
2025-07-17 20:48:06 -0600

    feat(telescope): migrate keymaps to plugin spec
    Moved Telescope keymap definitions from manual vim.keymap.set calls to
    the plugin specification's keys field for cleaner configuration and
    improved maintainability.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -13,6 +13,18 @@ return {
     "nvim-telescope/telescope-ui-select.nvim",
     "nvim-tree/nvim-web-devicons"
   },
+  keys = {
+    { '<leader>sh', '<cmd>Telescope help_tags<cr>', desc = '[S]earch [H]elp' },
+    { '<leader>sk', '<cmd>Telescope keymaps<cr>', desc = '[S]earch [K]eymaps' },
+    { '<leader>ff', '<cmd>Telescope find_files<cr>', desc = '[F]ind [F]iles' },
+    { '<leader>ss', '<cmd>Telescope builtin<cr>', desc = '[S]earch [S]elect Telescope' },
+    { '<leader>sw', '<cmd>Telescope grep_string<cr>', desc = '[S]earch current [W]ord' },
+    { '<leader>sg', '<cmd>Telescope live_grep<cr>', desc = '[S]earch by [G]rep' },
+    { '<leader>sd', function() require('telescope.builtin').diagnostics({ severity_bound = 0 }) end, desc = '[S]earch [D]iagnostics' },
+    { '<leader>sr', '<cmd>Telescope resume<cr>', desc = '[S]earch [R]esume' },
+    { '<leader>s.', '<cmd>Telescope oldfiles<cr>', desc = '[S]earch Recent Files ("." for repeat)' },
+    { '<leader><leader>', '<cmd>Telescope buffers<cr>', desc = '[ ] Find existing buffers' }
+  },
   config = function()
     require('telescope').setup {
       defaults = {
@@ -26,22 +38,5 @@ return {
         }
       }
     }
-    local builtin = require 'telescope.builtin'
-    vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-
-    vim.keymap.set('n', '<leader>sk',
-      builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles' })
-    vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-    vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-    vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-    vim.keymap.set('n', '<leader>sd', function()
-      require('telescope.builtin').diagnostics({ severity_bound = 0 }) 
-    end, { desc = '[S]earch [D]iagnostics' })
-    vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-    vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-    vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-    require("telescope").load_extension("ui-select")
   end
 }
