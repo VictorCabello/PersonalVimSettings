9027a20070302e156332bcd417b70008062d8e98
Victor Cabello <vmeca87@gmail.com>
2026-04-27 10:58:41 -0600

    feat: add ruff LSP, conform.nvim formatting, and pyright config

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -4,6 +4,7 @@ vim.pack.add({
   {src='https://github.com/nvim-treesitter/nvim-treesitter', version='main'},
   {src='https://github.com/iamcco/markdown-preview.nvim' },
   {src='https://github.com/OXY2DEV/markview.nvim' },
+  {src='https://github.com/stevearc/conform.nvim'},
 })
 
 
@@ -39,6 +40,27 @@ require "config/lsp_lua"
 vim.lsp.config['qmlls'] = {
   cmd = { 'qmlls6' }
 }
+
+vim.lsp.config('pyright', {
+  settings = {
+    python = {
+      analysis = {
+        typeCheckingMode = "standard",
+        autoImportCompletions = true,
+        useLibraryCodeForTypes = true,
+        diagnosticMode = "openFilesOnly",
+      },
+    },
+  },
+})
+
+vim.lsp.config('ruff', {
+  on_attach = function(client, _)
+    -- Pyright gestiona hover mejor; sin esto el hover alterna aleatoriamente
+    client.server_capabilities.hoverProvider = false
+  end,
+})
+
 vim.lsp.enable({
   'ts_ls',
   'pyright',
@@ -47,8 +69,8 @@ vim.lsp.enable({
   'qmlls',
   'copilot',
   'lemminx',
-  'copilot',
   'bashls',
+  'ruff',
  })
 
  vim.treesitter.language.register("bash", { "sh" })
@@ -65,6 +87,14 @@ vim.lsp.enable({
    end,
  })
 
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'python',
+  callback = function()
+    vim.bo.smartindent = false
+    vim.bo.cindent = false
+  end,
+})
+
 
 require("markview").setup({
     tables = {
@@ -78,6 +108,20 @@ require("markview").setup({
 
 
 -- Permite scroll horizontal para tablas anchas
-vim.opt.sidescrolloff = 8 
+vim.opt.sidescrolloff = 8
 -- Oculta los símbolos de markdown para limpiar la vista
-vim.opt.conceallevel = 2 
+vim.opt.conceallevel = 2
+
+require("conform").setup({
+  formatters_by_ft = {
+    python = { "ruff_format" },
+  },
+  format_on_save = {
+    timeout_ms = 2000,
+    lsp_fallback = false,
+  },
+})
+
+vim.keymap.set({ 'n', 'v' }, '<leader>f', function()
+  require("conform").format({ async = true, lsp_fallback = false })
+end, { desc = 'Format file or selection' })
